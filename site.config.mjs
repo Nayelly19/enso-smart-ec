5c9a6e0c3544e49a2b78c8363c0c4e9c3a0f110a
Nayelly19 <cedenoantonela88@gmail.com>
2026-06-19 13:31:20 -0500

    Add files via upload

---
+++ site.config.mjs
@@ -0,0 +1,13 @@
+const site = {
+  name: 'ENSO SMART',
+  title: 'ENSO SMART',
+  description: 'Un tema de documentación Astro limpio con contenido MDX, navegación rápida y estilo Tailwind CSS.',
+  siteUrl: 'https://compass-lilac-tau.vercel.app',
+  ogImage: '/og-image.png',
+  githubUrl: 'https://github.com/andreialba/compass',
+  navCtaLabel: 'Ver repositorio',
+  navCtaHref: 'https://github.com/andreialba/compass',
+  footerText: 'ENSO SMART es un tema de documentación Astro diseñado para documentación de producto y soporte.',
+};
+
+export default site;
