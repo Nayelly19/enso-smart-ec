75a865b258c160fa2b42a910b8ce490bf20c1b32
Nayelly19 <cedenoantonela88@gmail.com>
2026-06-19 14:21:42 -0500

    site.config.mjs

--- site.config.mjs
+++ site.config.mjs
@@ -1,13 +1,10 @@
-const site = {
-  name: 'ENSO SMART',
-  title: 'ENSO SMART',
-  description: 'Un tema de documentación Astro limpio con contenido MDX, navegación rápida y estilo Tailwind CSS.',
-  siteUrl: 'https://compass-lilac-tau.vercel.app',
-  ogImage: '/og-image.png',
-  githubUrl: 'https://github.com/andreialba/compass',
-  navCtaLabel: 'Ver repositorio',
-  navCtaHref: 'https://github.com/andreialba/compass',
-  footerText: 'ENSO SMART es un tema de documentación Astro diseñado para documentación de producto y soporte.',
+export default {
+  siteUrl: 'https://Nayelly19.github.io',
+  name: 'ENSO-SMART Ecuador',
+  title: 'ENSO-SMART Ecuador',
+  description: 'Sistema Inteligente de Predicción Climática y Riesgo de Inundaciones basado en IA',
+  githubUrl: 'https://github.com/Nayelly19/enso-smart-ec',
+  navCtaLabel: 'Documentación',
+  navCtaHref: '/getting-started',
+  footerText: 'ENSO-SMART Ecuador - Fase 1 MVP',
 };
-
-export default site;
